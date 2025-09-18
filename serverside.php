1552768f272eda7fce1212b6a8af921a085f5a5b
michael.genovia <genoviamichaelryan@gmail.com>
2025-09-18 17:47:17 +0400

    thirdly udpate files

--- serverside.php
+++ serverside.php
@@ -1,12 +1,19 @@
 <?php
+    include('./configuration.php');
+    header("Content-Type: application/json; charset=UTF-8");
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    
-if ($action === "read") {
-    $stmt = $pdo->query("SELECT * FROM upload");
-    echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
-}
+        $action = $_GET['action'] ?? '';
+        if ($action === "read") {
+            $stmt = $pdo->query("SELECT * FROM uploads");
+            echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+        }else {
+            $response = [ "status" => "error","message" => "Data error retrieval"];
+            // Encode to JSON
+            echo json_encode($response, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        }
 
+        
 
 }elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
     # code...
